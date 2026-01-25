f3dfd8f56156a0fd4d187207b8c469172799ed39
joaomatsuodev <joaomatsuodev@gmail.com>
2026-01-24 21:47:16 -0300

    layout base pronto

---
+++ src/components/Contact.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import { FaEnvelope, FaLinkedin, FaMapMarkedAlt, FaPhone } from 'react-icons/fa'
+
+const Contact = () => {
+    return (
+        <motion.div
+            initial={{ opacity: 0, y:50 }}
+            whileInView={{ opacity:1, y:0 }}
+            transition={{ duration: 1, ease: 'easeOut' }}
+            viewport={{ once: false, amount:0.2 }}
+            id='contact'
+            className='py-20 bg-dark-200'
+        >
+            <div className='container mx-auto px-6'>
+                <h2 className='text-3xl font-bold text-center mb-4'>
+                    Get In
+                    <span className='text-purple'>Touch</span>
+                </h2>
+                <p className='text-gray-400 text-center max-w-2xl mx-auto mb-16'>
+                    Have a project in mind or want to collaborate? Let's talk!
+                </p>
+
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-12 max-w-5xl mx-auto'>
+                    {/* Formulário para contato */}
+                    {/* <div>
+                        <form className='space-y-6'>
+                            <div>
+                                <label htmlFor="name" className='block text-gray-200'>Your Name</label>
+                                <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+                            </div>
+                             <div>
+                                <label htmlFor="email" className='block text-gray-200'>Email Address</label>
+                                <input className='w-full bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+                            </div>
+                            <div>
+                                <label htmlFor="message" className='block text-gray-200'>Your Message</label>
+                                <textarea className='w-full h-40 bg-dark-300 border border-dark-400 rounded-lg px-4 py-3 outline-none' type="text" />
+                            </div>
+                            <button type="submit" className='w-full px-6 py-3 bg-purple rounded-lg font-medium hover:bg-purle-700 transition duration-300 cursor-pointer'>
+                                Send
+                            </button>
+                        </form>
+                    </div> */}
+
+                    {/* My informations */}
+                    <div className='space-y-8'>
+                        <div className='flex items-start'>
+                            <div className='text-purple text-2xl mr-4'>
+                                <FaMapMarkedAlt />
+                            </div>
+                            <div>
+                                <h3 className='text-lg font-semibold mb-2'>Location</h3>
+                                <p className='text-gray-400'>São José dos Campos, São Paulo, Brazil</p>
+                            </div>
+                        </div>
+
+                         <div className='flex items-start'>
+                            <div className='text-purple text-2xl mr-4'>
+                                <FaEnvelope />
+                            </div>
+                            <div>
+                                <h3 className='text-lg font-semibold mb-2'>Email</h3>
+                                <p className='text-gray-400'>joaomatsuo@gmail.com</p>
+                            </div>
+                        </div>
+
+                        <div className='flex items-start'>
+                            <div className='text-purple text-2xl mr-4'>
+                                <FaPhone />
+                            </div>
+                            <div>
+                                <h3 className='text-lg font-semibold mb-2'>Phone</h3>
+                                <p className='text-gray-400'>+55 12 98109-9424</p>
+                            </div>
+                        </div>
+
+                        <div className='pt-4'>
+                            <h3 className='text-lg font-semibold mb-4'>Follow me in my Linkedin</h3>
+                            <div className='flex space-x-4'>
+                                <a href="#" className='w-12 h-12 rounded-full bg-dark-300 flex items-center justify-center text-white hover:bg-purple hover:text-white transition duration-300'>
+                                    <FaLinkedin />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Experience from '../components/Experience'
+import Contact from '../components/Contact'
+
+const Home = () => {
+  return (
+    <div> 
+      {/* Alocando os componentes */}
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact /> 
+    </div>
+  )
+}
+
+export default Home
