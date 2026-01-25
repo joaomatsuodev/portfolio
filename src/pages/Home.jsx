3502bb23405fd8badd5ecb55a505271b1de91468
joaomatsuodev <joaomatsuodev@gmail.com>
2026-01-24 22:09:28 -0300

    Removido o componente de Projetos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Skills from '../components/Skills'
-import Projects from '../components/Projects'
 import Experience from '../components/Experience'
 import Contact from '../components/Contact'
 
@@ -13,7 +12,6 @@ const Home = () => {
       <Hero />
       <About />
       <Skills />
-      <Projects />
       <Experience />
       <Contact /> 
     </div>
