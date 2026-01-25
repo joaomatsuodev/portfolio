dd4ec981d0155ac83bc841da0a27100a7b28c9aa
joaomatsuodev <joaomatsuodev@gmail.com>
2026-01-24 22:10:26 -0300

    Removendo Projetos da navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,11 +32,6 @@ const Navbar = () => {
                     <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
                 </a>
 
-                <a href="#projects" className='relative text-white/80 transition duration-300 hover:text-purple group'>
-                    <span>Projects</span>
-                    <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
-                </a>
-
                 <a href="#experience" className='relative text-white/80 transition duration-300 hover:text-purple group'>
                     <span>Experience</span>
                     <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
